29568d9ecfbb6a376f462c62002d1331e4fe9dad
phanq2222 <phanquang615@gmail.com>
2023-08-05 19:56:30 +0700

    declare class ClientHandlerThread

--- CHAT-APP/src/SERVER/ServerService.java
+++ CHAT-APP/src/SERVER/ServerService.java
@@ -1,9 +1,13 @@
 package SERVER;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -36,6 +40,73 @@ public class ServerService {
                 case "login" -> login(dataInputStream);
                 case "register" -> register(dataInputStream);
             }
+        }
+    }
+
+
+    private void login(DataInputStream dataInputStream) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        String username = dataInputStream.readUTF();
+        String password = dataInputStream.readUTF();
+
+        if (clients.containsKey(username)) {
+
+            if (clients.get(username).getIsLogin()) {
+                dataOutputStream.writeUTF("This account is online");
+                dataOutputStream.flush();
+                return;
+            }
+
+            ClientHandlerThread client = clients.get(username);
+            if (client.getPassword().equals(password)) {
+                client.setSocket(socket);
+                client.setLogin(true);
+
+                dataOutputStream.writeUTF("Login Successful");
+                dataOutputStream.flush();
+
+                client.start();
+
+                notifyOnlineUsers();
+            }
+        } else {
+            dataOutputStream.writeUTF("Invalid username or password");
+            dataOutputStream.flush();
+        }
+    }
+
+    class ClientHandlerThread extends Thread {
+
+        private final Lock lock;
+        private Socket socket;
+        private DataInputStream dataInputStream;
+
+        private DataOutputStream dataOutputStream;
+
+        final private String username;
+        final private String password;
+        private Boolean isLogin;
+
+        /**
+         * Tạo thread cho tài khoản vừa đăng ký rồi đăng nhập liền
+         *
+         * @param username tên đăng nhập
+         * @param password password
+         * @param isLogin  kiểm tra xem đã login chưa
+         * @param socket   socket của client
+         * @param lock     khoá synchronize
+         */
+        ClientHandlerThread(String username, String password, Boolean isLogin, Socket socket, Lock lock)
+                throws IOException {
+            this.username = username;
+            this.password = password;
+            this.isLogin = isLogin;
+            this.socket = socket;
+            this.lock = lock;
+
+            this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            this.dataInputStream = new DataInputStream(socket.getInputStream());
         }
     }
 }
